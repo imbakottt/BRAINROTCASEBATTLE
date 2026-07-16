871f6957e403df451b2ab8091d3da01ade7fba22
imbakottt <ivandche@gmail.com>
2026-07-16 23:17:50 +0300

    Delete brainrot_casebattle directory

--- brainrot_casebattle/config.js
+++
@@ -1,6 +0,0 @@
-// Заполните после создания проекта Supabase.
-// Эти значения безопасно использовать на клиенте при включённом RLS.
-window.BRAINROT_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
-};
